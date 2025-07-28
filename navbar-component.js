f7e1015e84d93b77c6818993a26801cdb5e03bb3
Keshav Kaushal <32331754+kesh44@users.noreply.github.com>
2025-07-28 23:21:13 +0100

    Update navigation and rename benefits to learn
    Renamed benefits.html to learn.html and updated all relevant links and text in index.html and navbar-component.js to reflect this change. Also removed .html extensions from internal navigation links for consistency.

--- navbar-component.js
+++ navbar-component.js
@@ -2,7 +2,7 @@ class NavbarComponent extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <nav class="navbar navbar-expand-sm navbar-light px-3">
-        <a class="navbar-brand d-flex align-items-center" href="index.html">
+        <a class="navbar-brand d-flex align-items-center" href="index">
           <img src="pp.jpg" alt="Logo" width="40" height="40" class="rounded-circle me-3">
           <span class="kpm">keshpianomusic</span>
         </a>
@@ -13,8 +13,8 @@ class NavbarComponent extends HTMLElement {
         </button>
         <div class="collapse navbar-collapse" id="mainNavbar">
           <ul class="navbar-nav ms-auto flex-row flex-lg-row align-items-center">
-            <li class="nav-item mx-2 d-flex align-items-center"><a class="nav-link" href="resources.html">Resources</a></li>
-            <li class="nav-item mx-2 d-flex align-items-center"><a class="nav-link" href="benefits.html">Benefits</a></li>
+            <li class="nav-item mx-2 d-flex align-items-center"><a class="nav-link" href="resources">Resources</a></li>
+            <li class="nav-item mx-2 d-flex align-items-center"><a class="nav-link" href="learn">Learn</a></li>
             <li class="nav-item mx-2 d-flex align-items-center">
               <a class="nav-link mt-1" href="https://youtube.com/@keshpianomusic" target="_blank" title="YouTube">
                 <i class="bi bi-youtube"></i>
