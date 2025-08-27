e2fd2c0dd383d02742356fe64864922a2dc08896
Keshav Kaushal <32331754+kesh44@users.noreply.github.com>
2025-08-27 15:02:45 +0100

    Revamp site navigation and add MIDI page
    Replaced the 'Resources' page with a new 'MIDI' page for downloading MIDI files and added testimonials. Updated navigation links and homepage layout to highlight MIDI and learning tools. Removed resources.html and added testimonial images for carousel display.

--- navbar-component.js
+++ navbar-component.js
@@ -13,7 +13,7 @@ class NavbarComponent extends HTMLElement {
         </button>
         <div class="collapse navbar-collapse" id="mainNavbar">
           <ul class="navbar-nav ms-auto flex-row flex-lg-row align-items-center">
-            <li class="nav-item mx-2 d-flex align-items-center"><a class="nav-link" href="resources">Resources</a></li>
+            <li class="nav-item mx-2 d-flex align-items-center"><a class="nav-link" href="midi">MIDI</a></li>
             <li class="nav-item mx-2 d-flex align-items-center"><a class="nav-link" href="learn">Learn</a></li>
             <li class="nav-item mx-2 d-flex align-items-center">
               <a class="nav-link mt-1" href="https://youtube.com/@keshpianomusic" target="_blank" title="YouTube">
